344b009989f2afb7e8073fdaabd9ec57e605ef15
Jeremy Lainé <jeremy.laine@m4x.org>
2018-02-10 17:35:08 +0100

    [examples] show multiple components

--- examples/ice-client.py
+++ examples/ice-client.py
@@ -12,9 +12,11 @@ STUN_SERVER = ('stun.l.google.com', 19302)
 WEBSOCKET_URI = 'ws://127.0.0.1:8765'
 
 
-async def offer():
+async def offer(options):
     connection = aioice.Connection(ice_controlling=True,
                                    stun_server=STUN_SERVER)
+    for i in range(1, options.components + 1):
+        connection.components.add(i)
     local_candidates = await connection.get_local_candidates()
 
     websocket = await websockets.connect(WEBSOCKET_URI)
@@ -41,9 +43,11 @@ async def offer():
     await connection.close()
 
 
-async def answer():
+async def answer(options):
     connection = aioice.Connection(ice_controlling=False,
                                    stun_server=STUN_SERVER)
+    for i in range(1, options.components + 1):
+        connection.components.add(i)
     local_candidates = await connection.get_local_candidates()
 
     websocket = await websockets.connect(WEBSOCKET_URI)
@@ -72,11 +76,12 @@ async def answer():
 
 parser = argparse.ArgumentParser(description='ICE tester')
 parser.add_argument('action', choices=['offer', 'answer'])
+parser.add_argument('--components', type=int, default=1)
 options = parser.parse_args()
 
 logging.basicConfig(level=logging.DEBUG)
 
 if options.action == 'offer':
-    asyncio.get_event_loop().run_until_complete(offer())
+    asyncio.get_event_loop().run_until_complete(offer(options))
 else:
-    asyncio.get_event_loop().run_until_complete(answer())
+    asyncio.get_event_loop().run_until_complete(answer(options))
