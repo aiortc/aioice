22e3aa2601563a32c88d31241578bab6a183096e
Jeremy Lainé <jeremy.laine@m4x.org>
2018-02-10 09:21:38 +0100

    [examples] make signalling server listen on 0.0.0.0

--- examples/signaling-server.py
+++ examples/signaling-server.py
@@ -27,5 +27,5 @@ async def echo(websocket, path):
 
 
 asyncio.get_event_loop().run_until_complete(
-    websockets.serve(echo, 'localhost', 8765))
+    websockets.serve(echo, '0.0.0.0', 8765))
 asyncio.get_event_loop().run_forever()
