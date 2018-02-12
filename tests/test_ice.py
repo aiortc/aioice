7cb9ab1e6c4a688e29ff657f087c4b0ff92cd0b4
Jeremy Lainé <jeremy.laine@m4x.org>
2018-02-12 09:35:15 +0100

    [tests] add test for Candidate.to_sdp

--- tests/test_ice.py
+++ tests/test_ice.py
@@ -88,10 +88,7 @@ class IceComponentTest(unittest.TestCase):
         self.assertEqual(pair.state, ice.CandidatePair.State.FAILED)
 
 
-class IceConnectionTest(unittest.TestCase):
-    def tearDown(self):
-        stun.RETRY_MAX = 7
-
+class IceCandidateTest(unittest.TestCase):
     def test_can_pair_ipv4(self):
         candidate_a = Candidate.from_sdp(
             '6815297761 1 udp 659136 1.2.3.4 31102 typ host generation 0')
@@ -122,6 +119,26 @@ class IceConnectionTest(unittest.TestCase):
         candidate_b = Candidate.from_sdp(
             '6815297761 2 udp 659136 1.2.3.4 12345 typ host generation 0')
         self.assertFalse(candidate_a.can_pair_with(candidate_b))
+
+    def test_from_to_sdp(self):
+        candidate = Candidate.from_sdp(
+            '6815297761 1 udp 659136 1.2.3.4 31102 typ host generation 0')
+        self.assertEqual(candidate.foundation, '6815297761')
+        self.assertEqual(candidate.component, 1)
+        self.assertEqual(candidate.transport, 'udp')
+        self.assertEqual(candidate.priority, 659136)
+        self.assertEqual(candidate.host, '1.2.3.4')
+        self.assertEqual(candidate.port, 31102)
+        self.assertEqual(candidate.type, 'host')
+        self.assertEqual(candidate.generation, 0)
+
+        self.assertEqual(candidate.to_sdp(),
+            '6815297761 1 udp 659136 1.2.3.4 31102 typ host generation 0')
+
+
+class IceConnectionTest(unittest.TestCase):
+    def tearDown(self):
+        stun.RETRY_MAX = 7
 
     @mock.patch('netifaces.interfaces')
     @mock.patch('netifaces.ifaddresses')
@@ -157,18 +174,6 @@ class IceConnectionTest(unittest.TestCase):
             '1.2.3.4',
             '2a02:0db8:85a3:0000:0000:8a2e:0370:7334',
         ])
-
-    def test_from_sdp(self):
-        candidate = Candidate.from_sdp(
-            '6815297761 1 udp 659136 1.2.3.4 31102 typ host generation 0')
-        self.assertEqual(candidate.foundation, '6815297761')
-        self.assertEqual(candidate.component, 1)
-        self.assertEqual(candidate.transport, 'udp')
-        self.assertEqual(candidate.priority, 659136)
-        self.assertEqual(candidate.host, '1.2.3.4')
-        self.assertEqual(candidate.port, 31102)
-        self.assertEqual(candidate.type, 'host')
-        self.assertEqual(candidate.generation, 0)
 
     def test_connect(self):
         conn_a = ice.Connection(ice_controlling=True)
