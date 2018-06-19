65063bde583d5f14f5f3dba5f42b8b6bf9ed54ce
Jeremy Lainé <jeremy.laine@m4x.org>
2018-06-19 14:39:20 +0200

    [tests] move invite_accept helper to test.utils

--- tests/test_ice.py
+++ tests/test_ice.py
@@ -7,26 +7,12 @@ from unittest import mock
 from aioice import Candidate, ice, stun
 
 from .turnserver import TurnServerProtocol
-from .utils import run
+from .utils import invite_accept, run
 
 
 async def delay(coro):
     await asyncio.sleep(1)
     await coro()
-
-
-async def invite_accept(conn_a, conn_b):
-    # invite
-    await conn_a.gather_candidates()
-    conn_b.remote_candidates = conn_a.local_candidates
-    conn_b.remote_username = conn_a.local_username
-    conn_b.remote_password = conn_a.local_password
-
-    # accept
-    await conn_b.gather_candidates()
-    conn_a.remote_candidates = conn_b.local_candidates
-    conn_a.remote_username = conn_b.local_username
-    conn_a.remote_password = conn_b.local_password
 
 
 class ProtocolMock:
--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,19 @@
 import asyncio
 
 
+async def invite_accept(conn_a, conn_b):
+    # invite
+    await conn_a.gather_candidates()
+    conn_b.remote_candidates = conn_a.local_candidates
+    conn_b.remote_username = conn_a.local_username
+    conn_b.remote_password = conn_a.local_password
+
+    # accept
+    await conn_b.gather_candidates()
+    conn_a.remote_candidates = conn_b.local_candidates
+    conn_a.remote_username = conn_b.local_username
+    conn_a.remote_password = conn_b.local_password
+
+
 def run(coro):
     return asyncio.get_event_loop().run_until_complete(coro)
