18def6884a07057b67faf14748d16f8b6ad638e9
Jeremy Lainé <jeremy.laine@m4x.org>
2018-06-30 17:33:48 +0200

    [package] mark as compatible with python 3.7

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     packages=['aioice'],
     install_requires=['netifaces'],
