de4d5815d89930e2dc443bc433435912c4ff0914
Jeremy Lainé <jeremy.laine@m4x.org>
2022-01-31 16:27:55 +0100

    [ice] do not bomb if connection_lost() is received more than once
    It may be possible for connection_lost() to be received more than once.
    To guard agains this scenario, only set the `__closed` Future once.

--- src/aioice/ice.py
+++ src/aioice/ice.py
@@ -170,9 +170,9 @@ class StunProtocol(asyncio.DatagramProtocol):
 
     def connection_lost(self, exc: Exception) -> None:
         self.__log_debug("connection_lost(%s)", exc)
-        if self.local_candidate is not None:
-            self.receiver.data_received(None, self.local_candidate.component)
-        self.__closed.set_result(True)
+        if not self.__closed.done():
+            self.receiver.data_received(None, None)
+            self.__closed.set_result(True)
 
     def connection_made(self, transport) -> None:
         self.__log_debug("connection_made(%s)", transport)
