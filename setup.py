3327dc562e76d62a012cf87ac0478e94c7ccb4d3
Jeremy Lainé <jeremy.laine@m4x.org>
2022-10-26 16:54:39 +0200

    [package] support Python 3.11

--- setup.py
+++ setup.py
@@ -35,9 +35,11 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     package_dir={"": "src"},
     package_data={"aioice": ["py.typed"]},
     packages=["aioice"],
     install_requires=["dnspython>=2.0.0", "netifaces"],
+    python_requires=">=3.7",
 )
