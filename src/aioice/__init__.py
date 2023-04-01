d7b6dfbc45ceb7b11dc786409af865047d09a159
Jeremy Lainé <jeremy.laine@m4x.org>
2023-04-01 22:38:41 +0200

    Move package metadata to pyproject.toml

--- src/aioice/__init__.py
+++ src/aioice/__init__.py
@@ -1,9 +1,10 @@
 # flake8: noqa
 import logging
 
-from .about import __version__
 from .candidate import Candidate
 from .ice import Connection, ConnectionClosed, TransportPolicy
+
+__version__ = "0.9.0"
 
 # Set default logging handler to avoid "No handler found" warnings.
 logging.getLogger(__name__).addHandler(logging.NullHandler())
